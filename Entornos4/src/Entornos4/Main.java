f97228ed639f1059c5d6bcd296a68b3fe987e710
Waldo Gomez Moreno <Waldo@DESKTOP-V1SLM66>
2024-03-14 17:35:06 +0100

    Inicio

---
+++ Entornos4/src/Entornos4/Main.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Entornos4;
+
+/**
+ * 
+ * @author Waldo Gómez Moreno
+ */
+
+public class Main { 
+
+    public static void main(String[] args) {
+
+        eCesta cesta;
+
+        cesta = new eCesta("Apellidos del alumno",100, "dni del alumno", 10);
+
+        NuevoMetodo(cesta); 
+
+    }
+
+    public static void NuevoMetodo(eCesta cesta) {
+        try
+            
+        {
+            
+            cesta.comprar(50);
+
+        } catch (Exception e)
+
+        {
+
+            System.out.println("No se puede pagar  ");
+
+        }
+
+        try
+
+        {
+
+            System.out.println("Solicitar actualización de bono");
+
+            cesta.ActualizarBono(100);
+
+        } catch (Exception e)
+
+        {
+
+            System.out.println("Error al recargar");
+
+        }
+
+        double bonoactual = cesta.bonificacion();
+
+        System.out.println("Su bono actual es  "+ bonoactual );
+    }
+
+}
